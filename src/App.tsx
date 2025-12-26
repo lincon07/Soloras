916bba84efc44f449f18c32bc559d8a1acff9ee0
Poncho <lincon07@users.noreply.github.com>
2025-12-26 15:12:42 -0800

    auto update working

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import { check } from "@tauri-apps/plugin-updater";
+import { check, Update } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process"
+import { toast } from "sonner";
+import { Button } from "./components/ui/button";
+import { json } from "node:stream/consumers";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -12,16 +16,48 @@ function App() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
   }
-const [response, setResponse] = useState<any>("");
+  const [response, setResponse] = useState<any>("");
+
+
+  const handleDownloadAndInstallAndReLAUNCH = async (update: Update) => {
+    await update?.downloadAndInstall();
+    await relaunch()
+  }
   const handleCheckForUpdates = async () => {
     const isUpdateAvail = await check();
+
+    if (isUpdateAvail != null) {
+      // update is available
+      console.log("Update available: ", isUpdateAvail);
+
+      console.log("Downloading and installing update...");
+      toast.success(`Update ${isUpdateAvail?.version} availible, would you likee to inststall now`, {
+        description: isUpdateAvail?.date,
+        action: {
+          label: "Install",
+          onClick: async () => {
+            toast.promise(handleDownloadAndInstallAndReLAUNCH(isUpdateAvail), {
+              loading: "Downloading and installing",
+              success: "Updadate installed, resgafting app",
+              error: "failed to install update",
+            }
+          )
+          }
+        }
+      })
+
+    } else {
+      console.log("No update available");
+    }
     console.log(isUpdateAvail);
     setResponse(isUpdateAvail);
   };
+
+
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
-
+      v2.0.0 should not see if update is worked
       <div className="row">
         <a href="https://vite.dev" target="_blank">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
@@ -49,7 +85,7 @@ const [response, setResponse] = useState<any>("");
         />
         <button type="submit">Greet</button>
       </form>
-      <button onClick={handleCheckForUpdates}>Check for updates</button>
+      <Button onClick={handleCheckForUpdates}>Check for updates</Button>
       <p>{greetMsg}</p>
       <p>Update available: {response ? "Yes" : "No"}</p>
     </main>
